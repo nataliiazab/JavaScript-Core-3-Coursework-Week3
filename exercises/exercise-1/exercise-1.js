8f44272e2952d973d3dcd35d84a1133920c64bfc
nataliizab <nataliia.zab@gmail.com>
2023-02-16 22:17:09 +0000

    Update exercise-1.js

--- exercises/exercise-1/exercise-1.js
+++ exercises/exercise-1/exercise-1.js
@@ -4,10 +4,18 @@ const personOne = {
   favouriteFood: "Spinach",
 };
 
-function introduceYourself(___________________________) {
+// - What is the syntax to destructure the object `personOne` in exercise-1.js?
+// let {name, age, favouriteFood} = personOne;
+
+
+
+// - Update the argument of the function `introduceYourself` to use destructuring on the object that gets passed in.
+function introduceYourself(personN) {
   console.log(
-    `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
+    `Hello, my name is ${personN.name}. I am ${personN.age} years old and my favourite food is ${personN.favouriteFood}.`
   );
 }
 
 introduceYourself(personOne);
+
+
