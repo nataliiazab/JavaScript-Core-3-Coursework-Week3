e80b0989034dd7c45e2b938a2a308747c29d5665
nataliizab <nataliia.zab@gmail.com>
2023-02-16 22:53:08 +0000

    Update exercise-2.js

--- exercises/exercise-2/exercise-2.js
+++ exercises/exercise-2/exercise-2.js
@@ -70,3 +70,61 @@ let hogwarts = [
     occupation: "Teacher",
   },
 ];
+
+// // Task 1
+
+// // - In `exercise-2.js` write a program that will take the `hogwarts` array
+// // as input and display the names of the people who belong to the Gryffindor house.
+// // - Use array destructuring to extract the values you need out of the array.
+
+let expectedNames = [];
+
+// // //without destructuring
+// // function findGryffindors(myFunction) {
+// // for (let i = 0; i < myFunction.length; i++) {
+// //   if (myFunction[i].house === "Gryffindor") {
+// //     let name = myFunction[i].firstName + " " + myFunction[i].lastName;
+// //     expectedNames.push(name);
+// //   }
+
+// // }
+// // console.log (expectedNames);
+// // }
+
+// // findGryffindors(hogwarts);
+
+//with destrcturing
+
+for (const {firstName, lastName, house} of hogwarts) {
+if (house === "Gryffindor") {
+  expectedNames.push(firstName + " " + lastName)
+}}
+
+console.log (expectedNames);
+
+// Expected result
+
+// Harry Potter
+// Ron Weasley
+// Hermione Granger
+// Minerva McGonagall
+// Albus Dumbledore
+
+//Task2
+
+// - In `exercise-2.js` write a program that will take the `hogwarts` array as
+// input and display the names of teachers who have pets.
+// - Use array destructuring to extract the values you need out of the array.
+
+let teachersWithPets = [];
+
+for (const { firstName, lastName, pet, occupation } of hogwarts) {
+  if (occupation === "Teacher" && pet != null) {
+    teachersWithPets.push(firstName + " " + lastName);
+  }
+}
+
+console.log(teachersWithPets);
+
+// Expected result
+// Albus Dumbledore
